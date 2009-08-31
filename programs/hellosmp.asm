d34968dc3921f506330d01c248258744e3fb5a3f
Ian Seyler <iseyler@returninfinity.com>
2009-08-31 18:42:25 +0000

    cleanup and added comments

--- programs/hellosmp.asm
+++ programs/hellosmp.asm
@@ -21,27 +21,31 @@ no_more:
 ret					; Return to OS
 
 
+; This procedure will be executed by each of the processors
+; It requires mutually exclusive access while it creates the string and prints to the screen
+; We must insure that only one CPU at a time can execute this code, so we employ a 'spinlock'.
 ap_print_hello:
-	bt word [mutex], 0	; Check if the lock is open
+	bt word [mutex], 0	; Check if the mutex is free
 	jnc ap_print_hello	; If not check it again
 
-	lock		; The lock was open, try to lock it
-	btr word [mutex], 0	; Set the lock
-	jnc ap_print_hello	; Check if we were able to lock it, if not the check again
-	
-	mov rsi, hellofrom
-	call os_print_string
-	call os_smp_get_id
+	lock			; The mutex was free, lock the bus
+	btr word [mutex], 0	; Try to grab the mutex
+	jnc ap_print_hello	; Jump if we were unsuccessful
 
+	mov rsi, hellofrom
+	call os_print_string	; Print the "hello from" string
+
+	call os_smp_get_id	; Get the local APIC ID
 	mov rdi, tempstring
 	mov rsi, rdi
-	call os_int_to_string
-	call os_print_string
-	
-	bts word [mutex], 0	; Release the lock
+	call os_int_to_string	; Convert it to a string
+	call os_print_string	; Print the APIC ID
+
+	bts word [mutex], 0	; Release the mutex
 ret
 
 	hellofrom db '  Hello from CPU #', 0
-	mutex dw 1	; Our MUTual-EXclustion flag
+	mutex dw 1	; The MUTual-EXclustion flag
 
 tempstring:
+
