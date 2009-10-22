21d2d29369bcdc789a8f09e87074c1b6f03bcdcb
Ian Seyler <iseyler@returninfinity.com>
2009-10-22 18:24:40 +0000

    Working towards calling OS functions from a C application.

--- programs/testc.c
+++ programs/testc.c
@@ -1,15 +1,26 @@
-// gcc -o testc.o -c testc.c -nostdlib -nostartfiles -nodefaultlibs -O2 -fomit-frame-pointer
+// gcc -o testc.o -c testc.c -m64 -nostdlib -nostartfiles -nodefaultlibs -O2 -fomit-frame-pointer
 // ld -T app.ld -o testc.bin testc.o
+
+void print_string(char *string);
 
 int main(void)
 {
-	char *str = "Hello world, from C!", *ch;
-	unsigned short *vidmem = (unsigned short*) 0xb8000;
-	unsigned i;
+//	char *str = "Hello world, from C!", *ch;
+//	unsigned short *vidmem = (unsigned short*) 0xb8000;
+//	unsigned i;
 
-	for (ch = str, i = 0; *ch; ch++, i++)
-		vidmem[i] = (unsigned char) *ch | 0x0700;
+	print_string("This is a test.");
 
-	return 0x12345678;
+//	for (ch = str, i = 0; *ch; ch++, i++)
+//	{
+//		vidmem[i] = (unsigned char) *ch | 0x0700;
+//	}
+
+//	return 0x12345678;
+	return 0;
 }
 
+void print_string(char *string)
+{
+	//inline assembly here
+}
