52a3ccacaaf32416297b572e7b681ff27952f8df
Ian Seyler <iseyler@returninfinity.com>
2009-12-17 19:52:02 +0000

    Added C function for int_to_string

--- programs/testc1.c
+++ programs/testc1.c
@@ -4,12 +4,16 @@
 void b_print_string(const char *str);
 void b_print_char(const char chr);
 unsigned char b_input_wait_for_key(void);
+void b_int_to_string(unsigned long long nbr, char *str);
 
 int main(void)
 {
-	unsigned char tchar = 0x65;
-	b_print_char(tchar);
-	
+	unsigned char tchar = 0x65, tstring[25];
+//	b_print_char(tchar);
+
+	b_int_to_string(0xFFFFFFFFFFFFFFFF, tstring);
+	b_print_string(tstring);
+
 	b_print_string("Hello world, from C!\nHit a key: ");
 	tchar = b_input_wait_for_key();
 	
@@ -30,16 +34,21 @@ int main(void)
 // C passes the string address in RDI instead of RSI
 void b_print_string(const char *str)
 {
-	asm volatile ("call 0x00100010" :: "S"(str)); // Make sure string is passed in RSI
+	asm volatile ("call 0x00100010" : : "S"(str)); // Make sure string is passed in RSI
 }
 
 void b_print_char(const char chr)
 {
-	asm volatile ("call 0x00100018" :: "a"(chr)); // Make sure the char is passed in RAX
+	asm volatile ("call 0x00100018" : : "a"(chr));
 }
 
 // C call to os_input_key_wait
 unsigned char b_input_wait_for_key(void)
 {
 	asm volatile ("call 0x00100038");
+}
+
+void b_int_to_string(unsigned long long nbr, char *str)
+{
+	asm volatile ("call 0x001000C0" : : "a"(nbr), "D"(str));
 }
