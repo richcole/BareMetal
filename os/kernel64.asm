0b40ed9ce1efb01e3d9f856a6b2e6df09bfdb87d
Ian Seyler <iseyler@returninfinity.com>
2010-01-26 22:16:45 +0000

    Cleanup

--- os/kernel64.asm
+++ os/kernel64.asm
@@ -95,21 +95,25 @@ kernel_start:
 
 start:
 
-	call init_64		; After this point we are in a working 64-bit enviroment
+	call init_64			; After this point we are in a working 64-bit enviroment
 
-	call hdd_setup		; gather information about the harddrive and set it up
+	call hdd_setup			; Gather information about the harddrive and set it up
 
 ;	call init_pci
 
-	mov ax, 0x0016		; print the "ready" message
+	mov ax, 0x0016			; Print the "ready" message
 	call os_move_cursor
 	mov rsi, readymsg
 	call os_print_string
 
-	mov ax, 0x0018		; set the hardware cursor to the bottom left-hand corner
+	mov ax, 0x0018			; Set the hardware cursor to the bottom left-hand corner
 	call os_move_cursor
 	call os_show_cursor
 
+	mov rdi, taskdata		; Start the CLI
+	mov rax, os_command_line
+	stosq
+	jmp ap_sleep
 
 align 16
 
@@ -128,6 +132,8 @@ ap_clear:				; BSP and AP's start here on first start and after an exception
 	xor rax, rax			; Clear RAX to 0x0
 	stosq				; And store it to the task data
 
+	sti				; Re-enable interrupts (in case they were disabled)
+	
 	; We fall through to ap_sleep as align fills the space with No-Ops
 
 
@@ -135,7 +141,7 @@ align 16
 
 ap_sleep:				; AP's will normally be running here
 
-	; Reset the stack. Each CPU gets a 1024-byte unique stack location
+	; Reset the stack to a fresh state. Each CPU gets a 1024-byte unique stack location
 	xor rax, rax			; Clear RAX as the high 32 bits may contain data
 	mov rsi, [os_LocalAPICAddress]	; We would call os_smp_get_id here but the stack is not ...
 	add rsi, 0x20			; ... yet defined. It is safer to find the value directly.
@@ -144,7 +150,6 @@ ap_sleep:				; AP's will normally be running here
 	shl rax, 10			; shift left 10 bits for a 1024byte stack
 	add rax, stackbase		; stacks decrement when you "push", start at 1024 bytes in
 	mov rsp, rax			; Pure64 leaves 0x50000-0x9FFFF free so we use that
-	sti				; Re-enable interrupts
 
 	; Clear registers. Gives us a clean slate to work with
 	xor rax, rax			; aka r0
@@ -168,41 +173,45 @@ ap_sleep:				; AP's will normally be running here
 
 	; Check for a pending task
 	mov rsi, taskdata
-	shl rax, 4		; Quickly multiply RAX by 16 as each record (code+data) is 16 bytes (64bits x2)
-	add rsi, rax		; RSI now points to the taskdata entry for this CPU
+	shl rax, 4			; Quickly multiply RAX by 16 as each record (code+data) is 16 bytes (64bits x2)
+	add rsi, rax			; RSI now points to the taskdata entry for this CPU
 
 	; Test to see if there was a valid code location
-	mov rax, [rsi]		; Load the task code address into RAX
-	test rax, rax		; Same as a 'cmp rax, 0x0000000000000000' and saves a few bytes	
-	jne continue		; If it was not NULL then there is something to work on
+	mov rax, [rsi]			; Load the task code address into RAX
+	test rax, rax			; Same as a 'cmp rax, 0x0000000000000000' and saves a few bytes	
+	jne continue			; If it was not NULL then there is something to work on
 
 spin:
-	;pause			; Snooze for a bit
-	hlt			; Wait for a interrupt or "wakeup" IPI. No need to spin when there is nothing to do
-	mov rax, [rsi]		; Load the task code address into RAX
-	test rax, rax		; Same as a 'cmp rax, 0x0000000000000000' and saves a few bytes	
-	je spin			; If it was NULL then there is nothing to work on
+	;pause				; Snooze for a bit
+	hlt				; Wait for a interrupt or "wakeup" IPI. No need to spin when there is nothing to do
+	mov rax, [rsi]			; Load the task code address into RAX
+	test rax, rax			; Same as a 'cmp rax, 0x0000000000000000' and saves a few bytes	
+	je spin				; If it was NULL then there is nothing to work on
 
 continue:	
-	xchg rax, rbx		; Swap RAX and RBX since LODSQ uses RAX
-	add rsi, 8		; Increment RSI by the size of a QWORD (8 bytes)
-	mov rax, [rsi]		; Load the task data address/data variable into RAX
-	xchg rax, rbx		; Swap RAX and RBX again
-	xor rsi, rsi		; Clear RSI since we used it
+	xchg rax, rbx			; Swap RAX and RBX since LODSQ uses RAX
+	add rsi, 8			; Increment RSI by the size of a QWORD (8 bytes)
+	mov rax, [rsi]			; Load the task data address/data variable into RAX
+	xchg rax, rbx			; Swap RAX and RBX again
+	xor rsi, rsi			; Clear RSI since we used it
 
 	; If there is a pending task then call RAX
-	call rax		; At this point RAX holds the code location and RBX holds the data address/variable
+	call rax			; At this point RAX holds the code location and RBX holds the data address/variable
 
-	; Clear the pending task after execution. We will only get here is the task returned successfully.
-	call os_smp_get_id	; Get the APIC ID again. We could use the stack to save the id from earlier ...
-	mov rdi, taskdata	; ... but we don't know what the condition of the stack is on return.
-	shl rax, 4
-	add rdi, rax		; RDI points to the proper offset in taskdata
+	; Clear the pending task after execution. We will only get here if the task returned successfully.
+	xor rax, rax			; Clear RAX as the high 32 bits may contain data
+	mov rsi, [os_LocalAPICAddress]	; We would call os_smp_get_id here but the stack is not ...
+	add rsi, 0x20			; ... yet defined. It is safer to find the value directly.
+	lodsd				; Load a 32-bit value. We only want the high 8 bits
+	shr rax, 24			; Shift to the right and AL now holds the CPU's APIC ID
+	mov rdi, taskdata		; Load the starting address of the task data
+	shl rax, 4			; Quickly multiply RAX by 16 as each record (code+data) is 16 bytes (64bits x2)
+	add rdi, rax			; RDI points to the proper offset in taskdata
 	xor rax, rax
-	stosq			; Clear it
+	stosq				; Clear it
 
 	; Go back to sleep
-	jmp ap_sleep		; Reset the stack, clear the registers, and wait for something to work on
+	jmp ap_sleep			; Reset the stack, clear the registers, and wait for something to work on
 
 
 ; Includes
